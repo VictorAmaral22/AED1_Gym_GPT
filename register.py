f1c986a37e2db71465ac28b39f0777956d89cad5
victor-grandmasterwitcher <victortavamaral@gmail.com>
2023-09-18 14:03:24 -0300

    começando

--- register.py
+++ register.py
@@ -2,8 +2,11 @@ import graphics as gf
 import random
 
 winW = 1000
-winH = 1000
+winH = 800
 
 win = gf.GraphWin("Colisão", winW, winH)
+
+button = gf.Rectangle(gf.Point(0, 0), gf.Point(100, 100))
+button.draw(win)
 
 win.getMouse()
